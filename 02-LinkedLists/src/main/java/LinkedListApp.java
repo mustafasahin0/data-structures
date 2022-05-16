7210e2777430f49ac923857d26e8682941afcb84
msahin <msahin@switchfly.com>
2022-05-16 16:27:32 -0600

    added get and delete kth element from the list

--- 02-LinkedLists/src/main/java/LinkedListApp.java
+++ 02-LinkedLists/src/main/java/LinkedListApp.java
@@ -2,13 +2,14 @@ public class LinkedListApp {
     public static void main(String[] args) {
         MySinglyLinkedList list1 = new MySinglyLinkedList();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 11; i++) {
             list1.add(i);
         }
 
         list1.printNodes();
-        System.out.println();
-        System.out.println(list1.indexOf(4));
-        System.out.println(list1.size);
+        System.out.println("Last Kth item from last is: " + list1.getKthFromLast(4));
+        list1.removeKthFromLast(3);
+        list1.printNodes();
+
     }
 }
--- 02-LinkedLists/src/main/java/MySinglyLinkedList.java
+++ 02-LinkedLists/src/main/java/MySinglyLinkedList.java
@@ -39,32 +39,71 @@ public class MySinglyLinkedList {
             else System.out.print(current.value + "=>");
             current = current.next;
         }
+        System.out.println();
     }
 
     // deletes first item with the value
     void deleteNode(int value) {
         if (isEmpty()) System.out.println("No Values with the value send!!!");
-
         //first one
-
         Node current = head;
         Node previous = head;
         while (current != null) {
-            if (current.value == value) {
-                if (current == head) {
+            if (current.value == value) { // If you find a match
+                if (current == head) { // if the item to be deleted is the head
                     head = current.next;
-                    current.next = null;
-                } else if (current == tail) {
+                    current.next = null; // will be available for garbage collection
+                } else if (current == tail) { // if the item to be deleted is the tail
                     tail = previous;
                     previous.next = null;
-                } else {
+                } else { // if the item to be deleted is in the middle
                     previous.next = current.next;
                     current.next = null;
                 }
-                previous = current;
-                current = current.next;
+                size--; //decrease the size once you
             }
+            previous = current;
+            current = current.next;
         }
+    }
+
+    public int getKthFromLast(int k) {
+        Node ptr1 = head;
+        Node ptr2 = head;
+        for (int i = 0; i < k - 1; i++) {
+            ptr2 = ptr2.next;
+            if (ptr2 == null) return -1;
+        }
+        while (ptr2.next != null) {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+        return ptr1.value;
+    }
+
+    public void removeKthFromLast(int k) {
+        Node previousDelete = null;
+        Node ptr1 = head;
+        Node ptr2 = head;
+        for (int i = 0; i < k - 1; i++) {
+            ptr2 = ptr2.next;
+            if (ptr2 == null) System.out.println("No such value");
+        }
+        while (ptr2.next != null) {
+            previousDelete = ptr1;
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+        if (ptr1 == head) {
+            head = ptr1.next;
+            ptr1.next = null;
+            size--;
+        } else {
+            previousDelete.next = ptr1.next;
+            ptr1.next = null;
+            size--;
+        }
+
     }
 
     int indexOf(int value) {
