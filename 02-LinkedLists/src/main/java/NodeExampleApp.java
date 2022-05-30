7c754a46ca443c761cd050d928d0c674f4b9cb84
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-05-29 19:03:12 -0600

    Added LinkedLists App

--- 02-LinkedLists/src/main/java/NodeExampleApp.java
+++ 02-LinkedLists/src/main/java/NodeExampleApp.java
@@ -5,11 +5,19 @@ public class NodeExampleApp {
         MyNode nodeA = new MyNode("Node A");
         MyNode nodeB = new MyNode("Node B");
         MyNode nodeC = new MyNode("Node C");
+        MyNode nodeD = new MyNode("Node D");
+
 
         nodeA.next = nodeB;
+        nodeB.next = nodeC;
+        nodeC.next = nodeD;
 
-        System.out.println(nodeA);
-        System.out.println(nodeB);
-        System.out.println(nodeC);
+        //Iterating over the list
+        MyNode current = nodeA;
+        while (current != null) {
+            System.out.println(current.name);
+            current = current.next;
+        }
+
     }
 }
