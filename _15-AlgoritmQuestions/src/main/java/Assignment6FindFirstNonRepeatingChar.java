29350ea5cec04cf6a4a14e459c6d808387a94ccd
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-07-06 20:04:16 -0600

    added repeated char assignments

---
+++ _15-AlgoritmQuestions/src/main/java/Assignment6FindFirstNonRepeatingChar.java
@@ -0,0 +1,31 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Assignment6FindFirstNonRepeatingChar {
+
+    public static void main(String[] args) {
+        System.out.println(findFirstNonRepeatingChar("a green apple"));
+
+    }
+
+    public static char findFirstNonRepeatingChar(@NotNull String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        int count = 0;
+        char[] chars = str.toCharArray();
+        for (Character ch : chars) {
+            if (map.containsKey(ch)) {
+                count = map.get(ch);
+                map.put(ch, count + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+        for (Character ch : chars) {
+            if (map.get(ch) == 1) return ch;
+        }
+        return Character.MIN_VALUE;
+
+    }
+}
---
+++ _15-AlgoritmQuestions/src/main/java/Assignment7FindFirstRepeatedChar.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Assignment7FindFirstRepeatedChar {
+
+    public static void main(String[] args) {
+        System.out.println(findFirstRepeatingChar("a green apple"));
+    }
+
+    public static char findFirstRepeatingChar(String str) {
+        Set<Character> set = new HashSet<>();
+
+        char[] chars = str.toCharArray();
+
+        for (Character ch : chars) {
+            if(set.contains(ch)) return ch;
+            set.add(ch);
+        }
+
+        return Character.MIN_VALUE;
+    }
+}
