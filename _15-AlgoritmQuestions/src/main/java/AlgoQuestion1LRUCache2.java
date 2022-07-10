00e1ed93f366bd936e9f73d44adce46760dff892
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-07-10 13:59:48 -0600

    added LRU Cache 2 solution

--- _15-AlgoritmQuestions/src/main/java/AlgoQuestion1LRUCache2.java
+++ _15-AlgoritmQuestions/src/main/java/AlgoQuestion1LRUCache2.java
@@ -1,2 +1,16 @@
+import java.util.LinkedHashMap;
+
 public class AlgoQuestion1LRUCache2 {
+
+    public static void main(String[] args) {
+        LRUCache2 lhm = new LRUCache2(3);
+        lhm.put(1, 1);
+        lhm.put(2, 2);
+        lhm.put(3, 3);
+        lhm.put(4, 4);
+        lhm.entrySet().forEach(entry -> {
+            System.out.println("Key is: " + entry.getValue() + " Value is: " + entry.getValue());
+        });
+        System.out.println();
+    }
 }
