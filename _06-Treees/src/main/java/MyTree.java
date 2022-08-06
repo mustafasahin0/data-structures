b6c910ea5f107947b4795beec527f2cc8220e981
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-08-05 19:47:13 -0500

    Contains method added

--- _06-Treees/src/main/java/MyTree.java
+++ _06-Treees/src/main/java/MyTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyTree {
     public Node root;
 
@@ -7,27 +10,27 @@ public class MyTree {
     public MyTree(Node root) {
         this.root = root;
     }
-    public void insert(int value){
-        Node newNode=new Node(value);
-        if(root==null) {
-            root=newNode;
+
+    public void insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
             return;
         }
-        Node current=root;
-        while(true){
-            if(value<=current.value) {
-                if (current.leftChild==null) {
-                    current.leftChild=newNode;
+        Node current = root;
+        while (true) {
+            if (value <= current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = newNode;
                     break;
                 }
-                current=current.leftChild;
-            }
-            else  {
-                if (current.rightChild==null) {
-                    current.rightChild=newNode;
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = newNode;
                     break;
                 }
-                current=current.rightChild;
+                current = current.rightChild;
             }
 
         }
@@ -35,26 +38,55 @@ public class MyTree {
 
     // PreOrder Traversal of Tree  Root-Left-Right
 
-    public void preOrderTraversal(Node root){
-        if (root==null) return;
-        System.out.print(root.value+", ");
+    public void preOrderTraversal(Node root) {
+        if (root == null) return;
+        System.out.print(root.value + ", ");
         preOrderTraversal(root.leftChild);
         preOrderTraversal(root.rightChild);
     }
+
     // In-Order Traversal of Tree  Root-Left-Right
-    public void inOrderTraversal(Node root){
-        if (root==null) return;
+    public void inOrderTraversal(Node root) {
+        if (root == null) return;
         inOrderTraversal(root.leftChild);
-        System.out.print(root.value+", ");
+        System.out.print(root.value + ", ");
         inOrderTraversal(root.rightChild);
     }
+
     // Post-Order traversal  Left-Right-Root
-    public void postOrderTraversal(Node root){
-        if (root==null) return;
+    public void postOrderTraversal(Node root) {
+        if (root == null) return;
         postOrderTraversal(root.leftChild);
         postOrderTraversal(root.rightChild);
-        System.out.print(root.value+", ");
+        System.out.print(root.value + ", ");
 
+    }
+
+    // Assignment BreadFirst Traversal
+    public void levelOrderTraversal() {
+        if (root == null) return;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node toVisit = queue.poll();
+            System.out.println(toVisit.value + ", ");
+            if (toVisit.leftChild != null) queue.add(toVisit.leftChild);
+            if (toVisit.rightChild != null) queue.add(toVisit.rightChild);
+        }
+    }
+
+    public boolean contains(int value) {
+        if (root == null) return false;
+
+        Node current = root;
+
+        while (current != null) {
+            if (value < current.value) current = current.leftChild;
+            else if (value > current.value) current = current.rightChild;
+            else return true;
+        }
+        return false;
     }
 
 }
